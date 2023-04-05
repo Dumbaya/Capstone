18e32f610fb4ac1444787a514d28e1bd9cd77c47
obj <dhqudwo2976@naver.com>
2023-04-05 09:05:52 +0900

    간단한 메인ui구성(버튼1에 이벤트 추가)

--- src/App.js
+++ src/App.js
@@ -2,22 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function test(){
+    console.log("테스트용");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <div className="logo">
+          <div className="logo-img">
+            로고
+          </div>ㄴ
+        </div>
+        <div className="btns">
+          <button className="btn" onClick={test}>버튼1</button>
+          <button className="btn">버튼2</button>
+          <button className="btn">버튼3</button>
+        </div>
+      </div>
     </div>
   );
 }
