cb80c86f8fb13b8c1caa61c100ba90465deee42b
obj <dhqudwo2976@naver.com>
2023-04-05 09:36:58 +0900

    메인의 버튼들 컴포넌트로 분리

--- src/App.js
+++ src/App.js
@@ -2,25 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  function test(){
-    console.log("테스트용");
-  }
-
   return (
     <div className="App">
       <div className="main">
         <div className="logo">
           <div className="logo-img">
             로고
-          </div>ㄴ
+          </div>
         </div>
-        <div className="btns">
-          <button className="btn" onClick={test}>버튼1</button>
-          <button className="btn">버튼2</button>
-          <button className="btn">버튼3</button>
-        </div>
+        <Btns></Btns>
       </div>
+    </div>
+  );
+}
+
+function Btns(){
+
+  function test(){
+    console.log("테스트용");
+  }
+
+  return (
+    <div className="btns">
+      <button className="btn" onClick={test}>버튼1</button>
+      <button className="btn">버튼2</button>
+      <button className="btn">버튼3</button>
     </div>
   );
 }
