9f52a256d2bbe61516582dec296b09de13829cf7
obj <dhqudwo2976@naver.com>
2023-04-24 02:58:47 +0900

    홈 컴포넌트 분리

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <div className="main">
-        <div className="logo">
-          <div className="logo-img">
-            로고
-          </div>
-        </div>
-        <Btns></Btns>
-      </div>
+      <Home/>
     </div>
   );
 }
 
-function Btns(){
 
-  function test(){
-    console.log("테스트용");
-  }
-
-  return (
-    <div className="btns">
-      <button className="btn" onClick={test}>로그인, 냉장고</button>
-      <button className="btn">레시피</button>
-      <button className="btn">게시판</button>
-    </div>
-  );
-}
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Home(){
+    return (
+        <div className="Home">
+        <div className="logo">
+          <div className="logo-img">
+            로고
+          </div>
+        </div>
+        <Btns></Btns>
+      </div>
+    )
+}
+
+function Btns(){
+
+    function test(){
+      console.log("테스트용");
+    }
+  
+    return (
+      <div className="btns">
+        <button className="btn" onClick={test}>로그인, 냉장고</button>
+        <button className="btn">레시피</button>
+        <button className="btn">게시판</button>
+      </div>
+    );
+  }
+
+export default Home;
