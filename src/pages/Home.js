61c575d28aebf1e004286e468822f83cb93afd6c
obj <dhqudwo2976@naver.com>
2023-04-24 03:06:43 +0900

    줄정렬+각 페이지 코드 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,31 @@
 import React from "react";
 
-function Home(){
+function Home() {
     return (
         <div className="Home">
-        <div className="logo">
-          <div className="logo-img">
-            로고
-          </div>
+            <div className="logo">
+                <div className="logo-img">
+                    로고
+                </div>
+            </div>
+            <Btns></Btns>
         </div>
-        <Btns></Btns>
-      </div>
     )
 }
 
-function Btns(){
+function Btns() {
 
-    function test(){
-      console.log("테스트용");
+    function test() {
+        console.log("테스트용");
     }
-  
+
     return (
-      <div className="btns">
-        <button className="btn" onClick={test}>로그인, 냉장고</button>
-        <button className="btn">레시피</button>
-        <button className="btn">게시판</button>
-      </div>
+        <div className="btns">
+            <button className="btn" onClick={test}>로그인, 냉장고</button>
+            <button className="btn">레시피</button>
+            <button className="btn">게시판</button>
+        </div>
     );
-  }
+}
 
 export default Home;
---
+++ src/pages/List.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="Home">
+            <div className="logo">
+                <div className="logo-img">
+                    로고
+                </div>
+            </div>
+            <Btns></Btns>
+        </div>
+    )
+}
+
+export default Home;
