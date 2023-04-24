2ed0c889d9c7752181dec8ea12db94061656cdb5
obj <dhqudwo2976@naver.com>
2023-04-24 15:14:06 +0900

    테스트용 로그인폼 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, Link} from "react-router-dom";
 
 import './App.css';
 import Home from "./pages/Home";
+import LoginForm from "./pages/LoginForm";
 import List from "./pages/List";
 import Recipe from "./pages/Recipe";
 import Board from "./pages/Board";
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/LoginForm" element={<LoginForm />} />
         <Route path="/List" element={<List />} />
         <Route path="/Recipe" element={<Recipe />} />
         <Route path="/Board" element={<Board />} />
---
+++ src/pages/LoginForm.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function LoginForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // 여기에서 서버에 로그인 요청을 보내고, 로그인 성공 여부를 처리합니다.
+    console.log("username:", username, "password:", password);
+    setUsername("");
+    setPassword("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+export default LoginForm;
