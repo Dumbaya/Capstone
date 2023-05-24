2181455d43b1c53069538d473a2f0d2ba59e421b
obj <dhqudwo2976@naver.com>
2023-05-24 10:13:25 +0900

    모달 선택지 분할중

---
+++ src/pages/List/modal/Infor.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "../../../css/List.css";
+
+
+
+function Infor(props) {
+        return (
+            <div>
+                
+            </div>
+        );
+}
+
+export default Infor;
